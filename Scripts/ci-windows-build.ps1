559fd5693613a343f2a8fbb1aa5c468aafac79b3
Gabriel Le Breton <lebreton.gabriel@gmail.com>
2019-07-05 02:49:56 -0400

    No support for additional args

--- Scripts/ci-windows-build.ps1
+++ Scripts/ci-windows-build.ps1
@@ -4,6 +4,6 @@ Write-Host "$(date) Setting the right unity instance"-ForegroundColor green
 Get-UnitySetupInstance | Select-UnitySetupInstance -Latest
 
 Write-Host "$(date) Starting unity editor with method execution"-ForegroundColor green
-Start-UnityEditor -ExecuteMethod Build.Invoke -BatchMode -Quit -LogFile .\build.log -Wait -AdditionalArguments "-BuildArg1 -BuildArg2"
+Start-UnityEditor -ExecuteMethod Build.Invoke -BatchMode -Quit -LogFile .\build.log -Wait # -AdditionalArguments "-BuildArg1 -BuildArg2"
 
 Write-Host "$(date) Done with build. Output log saved to build.log"-ForegroundColor green
