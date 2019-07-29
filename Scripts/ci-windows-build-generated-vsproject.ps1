8e20b279008dd527f38712a4083eb3a95af5d07d
Gabriel Le Breton <lebreton.gabriel@gmail.com>
2019-07-28 22:57:10 -0400

    So 4() stands for execution, reverting previous refactor

--- Scripts/ci-windows-build-generated-vsproject.ps1
+++ Scripts/ci-windows-build-generated-vsproject.ps1
@@ -5,10 +5,10 @@ Write-Host "$(date) Starting msbuild script"-ForegroundColor green
 
 $build_target = 'WSAPlayer'
 $build_name = 'ExampleProjectName'
-pushd "./Builds/$(build_target)/$(build_name)"
+pushd "./Builds/$build_target/$build_name"
 
 $project_name = "unity3d-encrypted-appveyor-poc"
-$visualstudio_solution = "$(project_name).sln"
+$visualstudio_solution = "$project_name.sln"
 
 $buildPlatform = 'x64'
 $buildConfiguration = 'Release'
@@ -16,13 +16,13 @@ $appxPackageDir = "./AppxPackages/"
 $buildLogger = "C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
 
 Write-Host "$(date) Restoring nuget packages (this is usually done by visual studio ui automatically)"-ForegroundColor green
-msbuild $visualstudio_solution /m /t:restore /p:Configuration="$(buildConfiguration)" /p:Platform="$(buildPlatform)" /logger:"$(buildLogger)"
+msbuild $visualstudio_solution /m /t:restore /p:Configuration="$buildConfiguration" /p:Platform="$buildPlatform" /logger:"$buildLogger"
 
 Write-Host "$(date) Building msproject"-ForegroundColor green
-msbuild $visualstudio_solution /m /t:build /p:Configuration="$(buildConfiguration)" /p:Platform="$(buildPlatform)" /logger:"$(buildLogger)"
+msbuild $visualstudio_solution /m /t:build /p:Configuration="$buildConfiguration" /p:Platform="$buildPlatform" /logger:"$buildLogger"
 
-Write-Host "$(date) Generating appxPackage to '$(appxPackageDir)' based on following instructions: https://docs.microsoft.com/en-us/windows/uwp/packaging/auto-build-package-uwp-apps"-ForegroundColor green
-msbuild $visualstudio_solution /m /p:AppxBundlePlatforms="$(buildPlatform)" /p:AppxPackageDir="$(appxPackageDir)" /p:AppxBundle=Always /p:UapAppxPackageBuildMode=StoreUpload /logger:"$(buildLogger)"
+Write-Host "$(date) Generating appxPackage to '$appxPackageDir' based on following instructions: https://docs.microsoft.com/en-us/windows/uwp/packaging/auto-build-package-uwp-apps"-ForegroundColor green
+msbuild $visualstudio_solution /m /p:AppxBundlePlatforms="$buildPlatform" /p:AppxPackageDir="$appxPackageDir" /p:AppxBundle=Always /p:UapAppxPackageBuildMode=StoreUpload /logger:"$buildLogger"
 
 popd
 Write-Host "$(date) Done with msbuild"-ForegroundColor green
--- Scripts/ci-windows-build.ps1
+++ Scripts/ci-windows-build.ps1
@@ -4,7 +4,7 @@ $ErrorActionPreference = "Stop"
 Write-Host "$(date) Starting build script"-ForegroundColor green
 Write-Host "$(date) Getting unity instance"-ForegroundColor green
 Get-UnitySetupInstance
-
+s
 $username = $env:UNITY_USERNAME
 $password = $env:UNITY_PASSWORD
 $serial = $env:UNITY_SERIAL
@@ -18,7 +18,7 @@ $secure_serial = ConvertTo-SecureString $serial -AsPlainText -Force
 #$build_target = 'StandaloneWindows64'
 $build_target = 'WSAPlayer'
 $build_name = 'ExampleProjectName'
-$build_path = "./Builds/$(build_target)/"
+$build_path = "./Builds/$build_target/"
 $build_log = ".\build.log"
 
 New-Item -Path $build_path -ItemType "directory"
@@ -29,14 +29,14 @@ Start-UnityEditor `
   -Serial $secure_serial `
   -BatchMode `
   -Quit `
-  -LogFile $(build_log) `
+  -LogFile $build_log `
   -ExecuteMethod BuildCommand.PerformBuild `
   -buildTarget $build_target `
   -Wait `
-  -AdditionalArguments "-customBuildTarget $(build_target) -customBuildName $(build_name) -customBuildPath $(build_path) -customBuildOptions AcceptExternalModificationsToPlayer"
+  -AdditionalArguments "-customBuildTarget $build_target -customBuildName $build_name -customBuildPath $build_path -customBuildOptions AcceptExternalModificationsToPlayer"
 # TODO: Try with free version too '-ForceFree `'
 
 Write-Host "$(date) Reading build logs"-ForegroundColor green
-Get-Content -Path $(build_log)
+Get-Content -Path $build_log
 
-Write-Host "$(date) Done with unity build. Output log saved to $(build_log)"-ForegroundColor green
+Write-Host "$(date) Done with unity build. Output log saved to $build_log"-ForegroundColor green
